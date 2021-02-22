72a25f24b42b0d1d64bbe021ebdefda377abc817
Hamza Elaasemy <hamza.elaasemy@imedia24.de>
2021-02-22 18:19:21 +0100

    setup

---
+++ src/container/Main.js
@@ -0,0 +1,10 @@
+import styles from "./Main.module.scss"
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './container/Main';
+import "./index.module.scss"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
