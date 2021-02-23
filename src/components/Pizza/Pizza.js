33c75c1a1cc6a6cae1e316d8c54a64eae4c0c2ad
Hamza Elaasemy <hamza.elaasemy@imedia24.de>
2021-02-23 09:58:29 +0100

    styling

--- src/components/Pizza/Pizza.js
+++ src/components/Pizza/Pizza.js
@@ -1,14 +1,27 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 import Arrows from "../../assets/arrows.svg"
 import Pouder from "../../assets/pouder.svg"
 import PizzaImg from "../../assets/pizza.png"
+
+import AnimatedNumber from "animated-number-react";
 
 import styles from "./pizza.module.scss"
 
 const Pizza = () => {
     const sizes = ['Small', 'Medium', 'Xxl'];
     const [active, setActive] = useState('Medium');
+    const [price, setPrice] = useState('10.5');
+    const priceFormated = Number(price).toFixed(2)
+    useEffect(() => {
+        if (active == 'Small') {
+            setPrice('7')
+        } else if (active == 'Xxl') {
+            setPrice('14')
+        } else {
+            setPrice('10.5')
+        }
+    }, [active])
     return (
         <>
             <div className={styles.wrapper}>
@@ -26,12 +39,17 @@ const Pizza = () => {
                     </fieldset>
                 </div>
                 <div className={styles.price}>
-                    <h1><sup>$</sup>10.50</h1>
-                    <img src={Arrows}/>
+                    <h1><sup>$</sup>
+                        <AnimatedNumber
+                            value={priceFormated}
+                        />
+                        
+                    </h1>
+                    <img src={Arrows} />
                 </div>
                 <div className={styles.pizza}>
                     <img src={Pouder} />
-                    <img src={PizzaImg}/>
+                    <img src={PizzaImg} />
                 </div>
             </div>
         </>
